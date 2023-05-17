f73ee35d55addfff50199e8dcf517c8647cc1b91
zhuangyan zu <zuzhuangyan@gmail.com>
2023-05-17 15:27:08 +0800

    update 1526

--- easyChatGPT-google_login/examples/main.py
+++ easyChatGPT-google_login/examples/main.py
@@ -6,7 +6,7 @@ import time
 import func_timeout
 import calendar
 import multiprocessing
-
+import re
 
 
 OPENAI_EMAIL = "aaaamiaoaaa@gmail.com"
@@ -23,18 +23,21 @@ def interact_gpt(text):
 if __name__ == '__main__':
 
     # data = pd.read_csv('./物流职业/extra_post.csv')
-    data = pd.read_csv('answer100.csv')
+    # data = pd.read_csv('answer100.csv')
+    data = pd.read_excel('gpt结果汇总1510.xlsx')
     labels = ['职业素养','通用能力','职业技能']
     # for label in labels:
     #     data[label] = None
 
-    for name in list(data['name'])[:101]:
+    for name in list(data['name']):
         for label in labels:
             print(name)
             if str(data.loc[data['name'] == name, label].values[0]) == 'nan':
                 demande = '十条'+ name +'最重要的'+ label+'并描述其内容'
                 ans = interact_gpt(demande)
-                data.loc[data['name'] == name, label] = ans + '\n' + interact_gpt('继续')
+                if len(re.findall("：", ans)) < 10:
+                    ans = ans + '\n' + interact_gpt('继续')
+                data.loc[data['name'] == name, label] = ans
                 time.sleep(5)
                 print(data.loc[data['name'] == name, label])
-                data.to_csv('answer100.csv')
+                data.to_csv('answer_after_union.csv')

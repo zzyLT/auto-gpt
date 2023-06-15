9908127f862556e632f39360fb3e2b30547f2bdc
zhuangyan zu <zuzhuangyan@gmail.com>
2023-06-15 09:48:18 +0800

    update results of the rest

--- main.py
+++ main.py
@@ -28,12 +28,13 @@ if __name__ == '__main__':
     三个维度合并到一起提问
     """
     # data = pd.read_excel('713 职业名称.xlsx')
-    # data['answer'] = np.NAN
+    data = pd.read_csv('extra.csv')
+    data['answer'] = np.NAN
 
-    min = 178
-    max = 356
+    min = 0
+    max = 999
 
-    data = pd.read_csv(f'./result/answer_{min}_{max}.csv')
+    # data = pd.read_csv(f'./result/answer_{min}_{max}.csv')
 
     time.sleep(30)
     list_name = list(data['职业名称'])
@@ -45,7 +46,7 @@ if __name__ == '__main__':
             if str(data.loc[data['职业名称'] == name, 'answer'].values[0]) == 'nan':
                 # demande = '十二条'+ name + '岗位' +'最重要的'+ label+'并描述其内容'
                 demande = '接下来你将充当物流行业的职业分析师，配合我完成一系列文本分析任务，要求：' \
-                         f'针对{name}，从专业素养、通用能力、专业技能三个维度进行分析，每个维度提取并分条列出不少于十条重要能力，每条能力说明需要提供10个技能关键词，并不需要描述性语句。' \
+                         f'针对{name}岗位，从专业素养、通用能力、专业技能三个维度进行分析，每个维度提取并分条列出不少于十条重要能力，每条能力说明需要提供10个技能关键词，并不需要描述性语句。' \
                           f'每个重要能力的名称需严格遵守格式要求即“XXX能力”，' \
                           f'并且能力名称不超过10个中文字符。' \
                           f'同时，每条能力的技能关键词要求用中文顿号隔开，并且每两个顿号之间为完整的技能关键词，并且每个技能关键词长度严格限定在4-6个中文字符之间。'
